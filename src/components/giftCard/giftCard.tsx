f59f35051dcda71d1e35bfd741af2ee9f1c41008
Ivan Kalitiul <ivankalytuk23@gmail.com>
2024-06-30 02:01:05 +0200

    gift cards

--- src/components/giftCard/giftCard.tsx
+++ src/components/giftCard/giftCard.tsx
@@ -3,7 +3,10 @@ import { FC } from "react";
 import './giftCard.scss'
 
 import sampleGiftPhoto from '@/assets/images/Sample Gift Photo.png'
-
+import dots from '@/assets/images/three dots.svg'
+import mark from '@/assets/images/markGrey.svg'
+import starGrey from '@/assets/images/StarGrey.svg'
+import starYellow from '@/assets/images/StarYellow.svg'
 
 const GiftCard: FC = () =>{
     return(
@@ -12,15 +15,49 @@ const GiftCard: FC = () =>{
 
             <div className="giftCard_name">Кавун базований свіжий Херсонський (1шт)</div>
 
-            <div className="giftCard_reating"></div>
+            <div className="giftCard_reating">
+                <div className="giftCard_reating_stars">
+                    <img src={starGrey} alt="star" />
+                    <img src={starGrey} alt="star" />
+                    <img src={starGrey} alt="star" />
+                    <img src={starGrey} alt="star" />
+                    <img src={starGrey} alt="star" />
+                </div>
 
-            <div className="giftCard_views"></div>
+                <div className="giftCard_reating_reating">
+                    <img src={starYellow} alt="star" />
+                    <img src={starYellow} alt="star" />
+                    <img src={starYellow} alt="star" />
+                    <img src={starYellow} alt="star" />
+                    <img src={starYellow} alt="star" />
+                </div>
+                
+            </div>
 
-            <div className="giftCard_tags"></div>
+            <div className="giftCard_views">1999 перегляди</div>
 
-            <div className="giftCard_additional"></div>
+            <div className="giftCard_tags">
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+                <div className="giftCard_tags_tag">#тег</div>
+            </div>
 
-            <div className="giftCard_mark"></div>
+            <div className="giftCard_additional"><img src={dots} alt="dots" /></div>
+
+            {/* Checkbox? */}
+            <div className="giftCard_mark"><img src={mark}  alt="mark" /></div>
         </div>
     )
 }
--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -3,9 +3,11 @@ import { FC } from "react";
 import './mainPage.scss'
 
 import search from '@/assets/images/Search.svg'
+import tick from '@/assets/images/tick.svg'
 
 import Filters from "./components/filters/filters";
 import GiftCard from "@/components/giftCard/giftCard";
+
 
 const MainPage: FC = () => {
     return (
@@ -15,7 +17,35 @@ const MainPage: FC = () => {
                 <Filters />
 
                 <div className="mainPage_giftContent">
+
+                    <div className="mainPage_giftContent_selectors">
+                        <div className="mainPage_giftContent_selector">
+                            <span>Новіші</span>
+                            <img src={tick} alt="tick" />
+
+                            <div className="mainPage_giftContent_selector_menu">Старіші</div>
+                        </div>
+
+                        <div className="mainPage_giftContent_selector">
+                            <span>Кращі</span>
+                            <img src={tick} alt="tick" />
+
+                            <div className="mainPage_giftContent_selector_menu">Гірші</div>
+                        </div>
+
+                        <div className="mainPage_giftContent_selector">
+                            <span>Популярніші</span>
+                            <img src={tick} alt="tick" />
+                            
+                            <div className="mainPage_giftContent_selector_menu">Непопулярні</div>
+                        </div>
+                    </div>
+
                     <div className="mainPage_giftContent_giftList">
+                        <GiftCard />
+                        <GiftCard />
+                        <GiftCard />
+                        <GiftCard />
                         <GiftCard />
                     </div>
                 </div>
