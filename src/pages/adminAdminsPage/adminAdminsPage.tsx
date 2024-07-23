09123dd6c2af178e179f0d21c326aec3d5a25ed8
Ivan Kalitiul <ivankalytuk23@gmail.com>
2024-07-23 17:03:28 +0200

    admins

--- src/pages/adminAdminsPage/adminAdminsPage.tsx
+++ src/pages/adminAdminsPage/adminAdminsPage.tsx
@@ -1,8 +1,28 @@
+import AdminPanelAdditional from "@/components/adminPanelAditional/adminPanelAdditional";
 import { FC } from "react";
+
+import './adminAdminsPage.scss'
+import Admin from "./components/admin/admin";
 
 const AdminAdminsPage: FC = () => {
     return(
-        <div>ADMINS</div>
+        <div className="adminAdminsPage">
+            <AdminPanelAdditional page="admins"/>
+
+            <div className="adminAdminsPage_content">
+                <div className="adminAdminsPage_heading">Пропозиції подарунків користувачів</div>
+
+                <div className="adminAdminsPage_search">
+                    <input type="text" className="inputText_preset" placeholder="Введіть пошту адміна"/>
+                </div>
+
+                <div className="adminAdminsPage_list">
+                    <Admin />
+                    <Admin />
+                    <Admin />
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ src/pages/adminAdminsPage/components/admin/admin.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+
+import './admin.scss' 
+
+import avatarSample from '@/assets/images/logoSample.jpg'
+import { Link } from "react-router-dom";
+
+const Admin: FC = () => {
+    return(
+        <div className="admin">
+            <div className="admin_left">
+                <img src={avatarSample} alt="avatar" />
+                <Link to={'/account/:user_id'}>Nickname</Link>
+            </div>
+
+            <div className="admin_right">
+                <div className="admin_status">3</div>
+                <button>-1</button>
+                <button>+1</button>
+            </div>
+        </div>
+    )
+}
+
+export default Admin
