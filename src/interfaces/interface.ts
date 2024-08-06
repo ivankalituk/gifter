9a124e77db39748fe0eda020e9f02bed8bb7e2f9
Ivan Kalitiul <ivankalytuk23@gmail.com>
2024-08-06 20:48:59 +0200

    redux

---
+++ src/interfaces/interface.ts
@@ -0,0 +1,7 @@
+export interface UserState {
+        user_nickName: null | string,
+        user_imgUrl: null | string,
+        user_role: null | number,
+        user_id: null | number,
+        user_email: null | string
+}
---
+++ src/redux/userSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserState } from "@/interfaces/interface";
+
+const  initialState: UserState = {
+    user_nickName: null,
+    user_imgUrl: null,
+    user_role: null,
+    user_id: null,
+    user_email: null
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser: (state, action: PayloadAction<UserState>) => {
+            state.user_nickName = action.payload.user_nickName;
+            state.user_imgUrl = action.payload.user_imgUrl;
+            state.user_role = action.payload.user_role;
+            state.user_id = action.payload.user_id;
+            state.user_email = action.payload.user_email
+        },
+        
+        clearUser: (state) => {
+            state.user_nickName = null;
+            state.user_imgUrl = null;
+            state.user_role = null;
+            state.user_id = null;
+            state.user_email = null;
+        },
+
+        setUserNickname: (state, action) => {
+            state.user_nickName = action.payload.user_nickName
+        },
+
+        setUserImgPath:  (state, action) => {
+            state.user_imgUrl = action.payload.user_imgUrl
+        }
+        
+    }
+})
+
+export const {setUser, clearUser, setUserNickname, setUserImgPath} = userSlice.actions
+export default userSlice.reducer
