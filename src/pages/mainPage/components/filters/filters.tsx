3fbfff9c826b43e2a2cbd8554bd3c82a28575eb0
Ivan Kalitiul <ivankalytuk23@gmail.com>
2024-08-13 20:11:50 +0200

    tagSearch

--- src/pages/mainPage/components/filters/filters.tsx
+++ src/pages/mainPage/components/filters/filters.tsx
@@ -49,7 +49,7 @@ const Filters: FC <FilterInterface>= ({filtersOpen, handleFiltersOpen}) =>{
 
 
     // для хранения выбранных тегов подарков
-    const [chosenTags, setChosenTags] = useState<string[] | null>(null)
+    const [chosenTags, setChosenTags] = useState<string[]>([])
 
 
 
@@ -59,18 +59,36 @@ const Filters: FC <FilterInterface>= ({filtersOpen, handleFiltersOpen}) =>{
     const [tagInputEnabled, setTagInputEnabled] = useState<boolean>(true)
 
 
+    // отловить изменение в поиске тегов 
     const handleTagInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>{    
         setTagInput(event.target.value)
-    
-        // тут же отправлять запрос на сервер по этому тегу
         
         setTagInputKey(tagInputKey + 1)
-        console.log(tags)
     }
 
-    const {data: tags, isFetched: tagsFetched} = useGetRequest({fetchFunc: () => getTagByInput({text: tagInput}), enabled: tagInputEnabled, key: [tagInputKey]})
+    // отловить выбор тега пользователем
+    const handleSearchTag = (text: string) => {
 
-    // сделать отображение данных
+        // сделать проверку на такие же теги
+        if(!chosenTags.includes(text)){
+            handleAddTag(text)
+        }
+
+        setTagInput('')
+        setTagInputKey(tagInputKey + 1)
+    }
+
+    // убрать тег по нажатию на него
+    const handleRemoveTag = (text: string) => {
+        setChosenTags(prevTags => prevTags.filter(tag => tag !== text))
+    }
+
+    const handleAddTag = (text: string) => {
+        setChosenTags(prevTags => [...prevTags, text])
+    }
+
+    // запрос на сервер для списка тегов по схожести
+    const {data: tags, isFetched: tagsFetched} = useGetRequest({fetchFunc: () => getTagByInput({text: tagInput}), enabled: tagInputEnabled, key: [tagInputKey]})
 
     return(
         <div className="filters">
@@ -81,16 +99,19 @@ const Filters: FC <FilterInterface>= ({filtersOpen, handleFiltersOpen}) =>{
                     <div className="filters_tagSearch_searchBar">
                         <img src={search} alt="search" />
                         <input type="text" placeholder="Введіть тег" value={tagInput} onChange={handleTagInputChange}/>
+
+                        {(tagsFetched && tags.length > 0) && <div className="filters_tagSearch_results">
+                            {tags.map((data: any, index: number) => (
+                                <button className="filters_tagSearch_result" onClick={() =>handleSearchTag(data.text)} key={index}>{data.text}</button>
+                                ))}
+                        </div>}
                     </div>
 
-                    <div className="filters_tagSearch_tags">
-                        <button className="tag">#ДляГеїв</button>
-                        <button className="tag">#ДляПедиків</button>
-                        <button className="tag">#ДляПедорастів</button>
-                        <button className="tag">#ДляНебінарніхХвойдоДрочерів</button>
-                        <button className="tag">#ДляГеївЩоГолосуютьЗаОПЗЖ</button>
-                        <button className="tag">#ХуйКартопляБалалайкаАнусТигрЯраняйка</button>
-                    </div>
+                    {chosenTags.length > 0 && <div className="filters_tagSearch_tags">
+                        {chosenTags.map((text: string, index: number) => (
+                            <button className="tag" key={index} onClick={() => handleRemoveTag(text)}>{text}</button>
+                        ))}
+                    </div>}
 
                 </div>
 
