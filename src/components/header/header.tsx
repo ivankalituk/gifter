de682bd5183ff42c299a4acb088e53b10e99d003
Ivan Kalitiul <ivankalytuk23@gmail.com>
2024-10-08 22:00:42 +0200

    burger redesign

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,20 @@
 import { FC, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import './header.scss'
 
 import logo from '@/assets/images/logo.svg'
+import accountBackground from '@/assets/images/burgerAccoungBack.jpg'
+import sampleLogo from '@/assets/images/logoSample.jpg'
 
+import mainPageSVG from '@/assets/images/burgerHome.svg'
+import suggestPageSVG from '@/assets/images/burgerSuggest.svg'
+import profilePageSVG from '@/assets/images/burgerProfile.svg'
+import settingsPageSVG from '@/assets/images/burgerSettings.svg'
+import adminPageSVG from '@/assets/images/burgerAdmin.svg'
+import blacklistPageSVG from '@/assets/images/burgerBlacklist.svg'
+import suggestListPageSVG from '@/assets/images/burgerSuggestList.svg'
+import reportsPageSVG from '@/assets/images/burgerReports.svg'
 
 import SearchBar from '@/components/header/components/searchBar/searchBar'
 import burger from '@/assets/images/burgerMenu.svg'
@@ -12,7 +22,8 @@ import Account from "./components/account/account";
 import SearchBarBig from "../searchBarBig/searchBarBig";
 import { useGetRequest } from "@/hooks/useGetReuquest";
 import { getGiftNameByName } from "@/api/gifts";
-import { giftName } from "@/interfaces/interface";
+import { giftName, RootState } from "@/interfaces/interface";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 interface HeaderProps {
     scrollCallback: (block: boolean) => void
@@ -47,6 +58,23 @@ const Header: FC<HeaderProps> = ({scrollCallback, nameSearchCallBack}) =>{
         nameSearchCallBack(text)
     }
 
+    const dispatch = useDispatch()
+    const useTypeSelector: TypedUseSelectorHook <RootState> = useSelector
+    const user = useTypeSelector((state) => state.user)
+
+    // 
+    // перенаправление на регистрацию или профиль
+    // 
+
+    const navigate = useNavigate()
+
+    const handleAccount = () => {
+        if (user.user_email === null){
+            navigate('/auth')
+        } else {
+            navigate('/profile')
+        }
+    }
 
     return (
         <header>
@@ -70,17 +98,75 @@ const Header: FC<HeaderProps> = ({scrollCallback, nameSearchCallBack}) =>{
 
                 <div className="burgerMenu_Menu">
                     <div className={!burgerMenu? "burgerMenu_Menu_background open" : "burgerMenu_Menu_background"} onClick={handleBurger}></div>
+                    
                     <div className={!burgerMenu? "burgerMenu_Menu_content open" : "burgerMenu_Menu_content" }>
-                        <div className="burgerMenu_auth">
-                            <Account />
-                        </div>
 
-                        <div className="burgerMenu_links">
-                            <Link to={'/'}><span>Головна</span></Link>
-                            <Link to={'/suggest'}><span>Запропонувати</span></Link>
-                            <Link to={'/profile'}><span>Ваш профіль</span></Link>
-                            <Link to={'/settings'}><span>Налаштування</span></Link>
+                    <div className="burgerMenu_account" onClick={handleAccount}>
+                        <img src={accountBackground} alt="accountBack" />
+
+                        <div className="burgerMenu_account_info_container">
+                            {user.user_email !== null &&<div className="burgerMenu_account_info_inner">
+                                
+                                <img  src={user.user_imgUrl !== null? 'http://localhost:1000/' + user.user_imgUrl : sampleLogo}/>
+
+                                <div className="burgerMenu_account_info_data">
+                                    <div className="burgerMenu_account_info_nickname">{user.user_nickName}</div>
+                                    <div className="burgerMenu_account_info_email">{user.user_email}</div>
+                                </div>
+                            </div>}
+
+                            {user.user_email === null && <div className="burgerMenu_account_info_inner">
+                                <div className="burgerMenu_account_info_registration">Реєстрація</div>
+                            </div>}
                         </div>
+                        
+                    </div>
+
+                    <div className="burgerMenu_links">
+                        <Link to={'/'}>
+                            <img src={mainPageSVG}/>
+                            <span>Головна</span>
+                        </Link>
+                        
+                        <Link to={'/suggest'}>
+                            <img src={suggestPageSVG}/>
+                            <span>Запропонувати</span>
+                        </Link>
+
+                        <Link to={'/profile'}>
+                            <img src={profilePageSVG}/>
+                            <span>Профіль</span>
+                        </Link>
+                        
+                        <Link to={'/settings'}>
+                            <img src={settingsPageSVG}/>
+                            <span>Налаштування</span>
+                        </Link>
+                    </div>
+
+                    <div className="burgerMenu_links">
+                        <Link to={'/'}>
+                            <img src={suggestListPageSVG}/>
+                            <span>Пропозиції</span>
+                        </Link>
+                        
+                        <Link to={'/'}>
+                            <img src={reportsPageSVG}/>
+                            <span>Скарги</span>
+                        </Link>
+
+                        <Link to={'/'}>
+                            <img src={adminPageSVG}/>
+                            <span>Адміни</span>
+                        </Link>
+                        
+                        <Link to={'/'}>
+                            <img src={blacklistPageSVG}/>
+                            <span>Чорний список</span>
+                        </Link>
+                    </div>
+
+
                     </div>
                 </div>
             </div>
