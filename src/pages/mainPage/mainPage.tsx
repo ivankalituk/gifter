6876bcd2109dc06a2bb25a10f5e4f27bd33625a4
Ivan Kalitiul <ivankalytuk23@gmail.com>
2024-12-13 01:24:53 +0200

    pages gifts

--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -28,17 +28,34 @@ const MainPage: FC <MainPageInterface>= ({scrollCallback, nameSearch, deleteSear
         scrollCallback(filtersOpen)
     }
 
+    const [page, setPage] = useState<number>(1)         //номер страницы
+
     const [giftKey, setGiftKey] = useState<number>(1)
     const [giftTags, setGiftTags] = useState<string[]>([])
 
     const [selector, setSelector] = useState<string>('За датою')              //селектор за датой, за названием, за рейтингом
 
     // НЕ ТОЛЬКО ТЕГИ НО И ФИЛЬТРЫ И ТЕКСТ
-    const {data: gifts, isFetched: giftsFetched} = useGetRequest<Gift[] | undefined>({fetchFunc: () => getAllGiftsByTags(giftTags, selector, nameSearch, user.user_id !== null? user.user_id : 0), key: [giftKey], enabled: true})
+    const {data: gifts, isFetched: giftsFetched} = useGetRequest<Gift[] | undefined>({fetchFunc: () => getAllGiftsByTags(giftTags, selector, nameSearch, user.user_id !== null? user.user_id : 0, page), key: [giftKey], enabled: true})
+
+    // следующая страица
+    const handleNextPage = () => {
+        setPage(page + 1)
+        setGiftKey(giftKey + 1)
+    }
+
+    // предьидущая страница
+    const handlePrevPage = () => {
+        if (page !== 1){
+            setPage(page - 1)
+            setGiftKey(giftKey + 1)
+        }
+    }
 
     // колбек для селектора
     const selecrotCallBack = (selectedSelector: string) => {
         setSelector(selectedSelector)
+        setPage(1)
         setGiftKey(giftKey + 1)
     }
 
@@ -46,6 +63,7 @@ const MainPage: FC <MainPageInterface>= ({scrollCallback, nameSearch, deleteSear
     const filtersCallBack = (tags: string[]) => {
         // сначала добавить новые фильтры, потом обновить ключ
         setGiftTags(tags)
+        setPage(1)
         setGiftKey(giftKey + 1)
     }
 
@@ -62,6 +80,12 @@ const MainPage: FC <MainPageInterface>= ({scrollCallback, nameSearch, deleteSear
                     {giftsFetched && <div className="mainPage_giftContent_giftList">
                         {gifts !== null && gifts !== undefined && gifts.map((data: any, index:number) => (<GiftCard scrollCallback = {scrollCallback} key={index} data={data}/>))}
                     </div>}
+
+                    <div className="mainPage_giftContent_pageSwich">
+                        <button className="button_preset" onClick={handlePrevPage}>Назад</button>
+                        <div>{page}</div>
+                        <button className="button_preset"onClick={handleNextPage}>Вперед</button>
+                    </div>
                 </div>
             </div>
         </div>
